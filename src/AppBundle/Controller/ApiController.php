a320f0a6cf0ca77422ef5c6fa6509a7b5549d4b3
jogich <ximofloridauni@hotmail.com>
2018-02-13 18:14:05 +0100

    add UserDeleteAction

--- src/AppBundle/Controller/ApiController.php
+++ src/AppBundle/Controller/ApiController.php
@@ -27,10 +27,37 @@ class ApiController extends Controller
         $serializer = new Serializer($normalizers, $encoders);
 
         if (empty($project)) {
-            $jsonContent = $serializer->serialize(array('code' => 400, 'message' => 'Projec doesnt exist'), 'json');
+            $jsonContent = $serializer->serialize(array('code' => 400, 'message' => 'Project doesnt exist'), 'json');
             $response = JsonResponse::fromJsonString($jsonContent);
         } else {
             $em->remove($project);
+            $em->flush();
+
+            $jsonContent = $serializer->serialize(array('code' => 200, 'Deleted succesfully'), 'json');
+            $response = JsonResponse::fromJsonString($jsonContent);
+        }
+
+        return $response;
+    }
+
+    /**
+     * @Route("/api/user/{id}/delete/", name="api-delete-user")
+     *
+     */
+    public function deleteUserAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $user = $em->getRepository('AppBundle:User')->find($id);
+
+        $encoders = array(new JsonEncoder());
+        $normalizers = array(new ObjectNormalizer());
+        $serializer = new Serializer($normalizers, $encoders);
+
+        if (empty($user)) {
+            $jsonContent = $serializer->serialize(array('code' => 400, 'message' => 'User doesnt exist'), 'json');
+            $response = JsonResponse::fromJsonString($jsonContent);
+        } else {
+            $em->remove($user);
             $em->flush();
 
             $jsonContent = $serializer->serialize(array('code' => 200, 'Deleted succesfully'), 'json');
