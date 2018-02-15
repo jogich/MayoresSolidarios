496a2eab63f8fe79e5b5b62a5c826e92870a40b4
Alfirex <gplp97@gmail.com>
2018-02-15 09:37:56 +0100

    Optimización de código y solucion de error nativo jquery

--- web/jquery/borrarFila.js
+++ web/jquery/borrarFila.js
@@ -1,20 +1,27 @@
 
     $(document).ready(function(){
+        var table;
+        if (location.href.indexOf("user")!=-1){
+          table = "user";
+        }else if (location.href.indexOf("project")!=-1){
+          table = "project";
+        }
+
         $(".eliminar").click(function(){
           var id=$(this).data("id");
           var elemento=$(this);
             $.ajax({
-              url: "/user/"+id+"/delete/",
+              url: "/"+table+"/1/delete/",
               type: 'DELETE',
               success: function(result){
               elemento.parent('td').parent('tr').hide(1000);
-              elemento.parent('td').parent('tr').remove(3000);
+              // elemento.parent('td').parent('tr').delay(9000).remove();
 
             }
           });
           $(document).ajaxError(function(){
-            $(".alert-warning").css("display", "block");
-            setTimeout(function(){  $(".alert-warning").css("display", "none"); }, 5000);
+            $(".alert-warning").show(1000);
+            setTimeout(function(){  $(".alert-warning").hide(1000); }, 5000);
           });
         });
     });
